36ac4049cc10360c1fe2bb07ffb51bd0b60560b6
Andrzej Wąsowski <wasowski@data.pl>
2015-09-16 19:57:33 +0200

    solutions for week 3

---
+++ 03/solutions/Gen.scala
@@ -0,0 +1,223 @@
+// Advanced Programming 2015
+// Andrzej Wasowski, IT Univesity of Copenhagen
+
+
+// 1. Introduction
+//
+// This file contains exercises 2--9
+//
+// This file is best worked on by reading exercise descriptions in the
+// corresponding PDF file.  Then find the place in the file where the stub for
+// the exercise is created and complete it.
+//
+// Before starting to work on the exercises, familiarize yourself with the the
+// content already in the file (it has been explained in chapter 8, but it is
+// useful to see it all together in one file).
+//
+// I compile this file calling
+// $ fsc State.scala Gen.scala
+//
+// Then you can load Exercise2.scala into the REPL to interactively test your
+// work. (Exercise2.scala is not so key, it just has all the useful imports in
+// the header, that make working with REPL more effcient for these exercises).
+//
+// The file is similar to the one created by Chiusano and Bjarnasson, but it
+// has small differences, since I created it myself working through the
+// chapter.
+
+package fpinscala.testing
+import fpinscala.state._
+import fpinscala.state.RNG._
+
+
+
+
+// A generator will use a random number generator RNG in its state, to create
+// random instances (but perhaps also some other staff)
+case class Gen[A] (sample :State[RNG,A]) {
+
+  // Let's convert generator to streams of generators
+  def toStream (seed :Long):Stream[A] =
+    Gen.state2stream(this.sample) (RNG.Simple(seed))
+  def toStream (rng :RNG):Stream[A] =
+    Gen.state2stream(this.sample) (rng)
+
+  // the book uses Stream.unfold, but apparently the standard library lacks this method
+
+  // Exercise 4 (Ex 8.5 second part)
+  //
+  // Hint: The standard library has the following useful function (List
+  // companion object):
+  //
+  // def fill[A](n: Int)(elem: ⇒ A): List[A]
+  //
+  // It is of course possible to implement a solution without it, but the
+  // result is ugly (you need to replicate the behavior of fill inside
+  // listOfN). Then note that State has a method sequence which allows to take
+  // a list of automata and execute their transitions as a sequence, feeding
+  // the output state of one as an input to the next.  This can be used to
+  // execute a series of consecutive generations, passing the RNG state around.
+
+  def listOfN (n :Int) :Gen[List[A]] =
+    Gen(State.sequence[RNG,A] (List.fill (n) (this.sample)))
+
+  // Exercise 5 (Ex. 8.6 [Chiusano, Bjarnasson 2015])
+  //
+  // So this is a solution that is ignoring the nice API that we developed.
+  // It builds the result from ground up.
+
+  def flatMap1[B] (f: A => Gen[B]) :Gen[B] =
+    Gen(State(rng =>
+        sample.run (rng) match { case (a,rng1) => f(a).sample.run (rng1) }))
+
+  // this is the ultimate solution if you recall that we already have flatMap
+  // for us which exactly interleaves two automata over the same state space.!
+
+  def flatMap[B] (f: A => Gen[B]) :Gen[B] =
+    Gen(sample.flatMap (a => f(a).sample))
+
+  // It would be convenient to also have map
+
+  def map[B] (f : A => B) :Gen[B] = this.flatMap (a => Gen.unit[B] (f(a)))
+
+  // Exercise 6 (Second part of Ex. 8.6)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap (n => this.listOfN (n))
+
+  // Exercise 7 (Ex. 8.7; I implemented it as a method, the book asks for a
+  // function, the difference is minor; you may want to have both for
+  // convenience)
+  //
+  // Hint: we already have a generator that emulates tossing a coin. Which one
+  // is it? Use flatMap with it.
+
+  def union (that :Gen[A]) :Gen[A] =
+    Gen.boolean.flatMap (b => if (b) this else that)
+
+  // Exercise 8 continues in the bottom of the file (in the companion object)
+}
+
+object Gen {
+
+  // A convenience function to convert states (automata) to streams (traces)
+  // It would be better to have it in State, but I am not controlling
+  // State.scala.
+
+  private  def state2stream[A] (s :State[RNG,A]) (seed :RNG) :Stream[A] =
+    s.run(seed) match { case (n,s1) => n #:: state2stream (s) (s1) }
+
+  // A generator for Integer instances
+
+  def anyInteger :Gen[Int] = Gen(State(_.nextInt))
+
+  // Exercise 2 (Ex. 8.4)
+  //
+  // Hint: Before solving the exercise study the type \lstinline{Gen} in
+  // \texttt{Gen.scala}. Then, think how to convert a random integer to a
+  // random integer in a range.  Then recall that we are already using
+  // generators that are wrapped in \texttt{State} and the state has a
+  // \lstinline{map} function.
+
+  def choose (start :Int, stopExclusive :Int) :Gen[Int] =
+    Gen[Int]( State(RNG.nonNegativeInt).map
+      (n => start + n % (stopExclusive - start)))
+
+  // Exercise 3 (Exercise 8.5, part one)
+  //
+  // Hint: The \lstinline{State} trait already had \lstinline{unit}
+  // implemented.
+
+  def unit[A] (a : =>A) :Gen[A] = Gen (State.unit[RNG,A](a))
+
+  // Hint: How do you convert a random integer number to a random Boolean?
+  // Alternatively: do we already have a random generator for booleans? Could
+  // we wrap it in.
+
+  // one solution
+  def boolean1 :Gen[Boolean]  =
+    Gen[Boolean] (State[RNG,Int](_.nextInt).map (n => n % 2 == 0))
+
+  // Another solution.
+  // This seems to be slightly more elegant, and it does proper reuse (Ex. 8.5)
+  def boolean :Gen[Boolean]  = Gen (State[RNG,Boolean](RNG.boolean))
+
+  // Hint: Recall from Exercise1.scala that we already implemented a random
+  // number generator for doubles.
+
+  def double :Gen[Double] = Gen (State[RNG,Double](RNG.double))
+
+  // (Exercise 4 is found in the Gen class above)
+
+  // Exercise 8 (Ex. 8.8 in Chiusano and Bjarnasson)
+  //
+  // Hint: Recall that we already have a generator of random double numbers
+  // from range (0;1); See Exercise 3. First translate weights to
+  // probabilities. Then use our generator of doubles to simulate an unfair
+  // coin with flatMap.
+
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val pivot = g1._2 / (g1._2+g2._2)
+    double.flatMap { d => if (d <= pivot) g1._1 else g2._1 }
+  }
+  //
+  // Nice test idea for the above: create 1.0:2.0 boolean generator, translate
+  // to stream, and try longer and longer prefixes to see if the law of big
+  // numbers works
+  //
+  // (Exercise 9 is found below in class Prop)
+
+}
+
+// This is the Prop type implemented in [Chiusano, Bjarnasson 2015]
+
+object Prop {
+
+  type TestCases = Int
+  type SuccessCount = Int
+  type FailedCase = String
+
+  // the type of results returned by property testing
+
+  sealed trait Result { def isFalsified: Boolean }
+  case object Passed extends Result { def isFalsified = false }
+  case class Falsified(failure: FailedCase,
+    successes: SuccessCount) extends Result {
+      def isFalsified = true
+  }
+  case object Proved extends Result { def isFalsified = false }
+
+  def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop {
+    (n,rng) => as.toStream(rng).zip(Stream.from(0)).take(n).map {
+      case (a,i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
+  }
+
+  def buildMsg[A](s: A, e: Exception): String =
+    s"test case: $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
+}
+
+import Prop._
+
+case class Prop (run :(TestCases,RNG) => Result) {
+
+  // (Exercise 9)
+
+  def && (that :Prop) :Prop = Prop {
+    (tcs,rng) => run (tcs,rng) match {
+      case r => if (r.isFalsified) r else that.run(tcs,rng)
+    }
+  }
+
+  def || (that :Prop) :Prop = Prop {
+    (tcs,rng) => run (tcs,rng) match {
+      case r => if (r.isFalsified) that.run(tcs,rng) else r
+    }
+  }
+
+}
+
+// vim:cc=80:foldmethod=indent:foldenable
